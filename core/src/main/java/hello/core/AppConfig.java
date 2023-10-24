efd5833d775f13879a7a2e0c2f836d0ec995c29d
yangrangrang <gkssk2309@gmail.com>
2023-10-24 19:24:51 +0900

    appConfig refactoring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,12 +12,24 @@ import hello.core.order.OrderServiceImpl;
 // 애플리케이션 전체를 설정하고 구성
 public class AppConfig {
 
+    // memberService 역할
     public MemberService memberService() {
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // memberRepository 역할
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // orderService 역할
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // discountpolicy 역할
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
