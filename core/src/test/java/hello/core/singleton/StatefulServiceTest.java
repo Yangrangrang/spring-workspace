4b53c5ed0948b14e09f931d01af859e3c958f859
yangrangrang <gkssk2309@gmail.com>
2023-10-30 22:56:47 +0900

    Shared fields (Use)

--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,16 +16,18 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA : A사용자가 10000원을 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         // ThreadB : B사용자가 10000원을 주문
-        statefulService1.order("userB", 20000);
+        int userBPrice = statefulService1.order("userB", 20000);
 
         // ThreadA : 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+//        System.out.println("price = " + price);
         // 사용자A는 10000원 결제인데 20000원이 나옴. 같은 객체를 사용하는 부분은 변동되는 값이 없어야 한다. (price는 공유되는 필드)
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        System.out.println("price = " + userAPrice);
+
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
