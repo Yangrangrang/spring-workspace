b54f3062926ed9fffe7ef342ea38f56d53820a50
yangrangrang <gkssk2309@gmail.com>
2023-11-03 10:05:50 +0900

    componenyscan -basePackages

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.FilterType;
 // @Component 붙은 빈을 다 찾아서 자동으로 스프링 빈에 등록
 @ComponentScan (
         // Configuration 어노테이션 붙은건 뺄꺼야 (실무에서는 굳이 이렇게 안한다)
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
