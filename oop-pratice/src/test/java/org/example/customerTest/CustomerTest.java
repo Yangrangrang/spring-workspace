64e2ea8efd85cc870e7c3bf33ce2038ab1e56ce6
yangrangrang <gkssk2309@gmail.com>
2023-11-12 22:13:31 +0900

    Fast - part1.Ch03.05 / customerTest(2)

--- oop-pratice/src/test/java/org/example/customerTest/CustomerTest.java
+++ oop-pratice/src/test/java/org/example/customerTest/CustomerTest.java
@@ -1,6 +1,16 @@
 package org.example.customerTest;
 
+import org.assertj.core.api.Assertions;
+import org.example.customer.Cooking;
+import org.example.customer.Customer;
+import org.example.customer.Menu;
+import org.example.customer.MenuItem;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 /**
  * 음식점에서 음식 주문하는 과정 구현
@@ -22,8 +32,14 @@ import org.junit.jupiter.api.Test;
  */
 public class CustomerTest {
 
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Cooking cooking = new Cooking();
 
+        assertThatCode(()->customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
--- oop-pratice/src/test/java/org/example/customerTest/MenuTest.java
+++ oop-pratice/src/test/java/org/example/customerTest/MenuTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class MenuTest {
 
@@ -19,5 +20,15 @@ public class MenuTest {
 
         MenuItem menuItem = menu.choose("돈까스");
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+
+    @DisplayName("메뉴판에 없는 메뉴를 선택할시 예외를 반환")
+    @Test
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+
+        assertThatCode(() -> menu.choose("통닭"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 이름 입니다.");
     }
 }
