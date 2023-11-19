ec98e390767862d3226c74e176fcc7fd947e3cc3
yangrangrang <gkssk2309@gmail.com>
2023-11-19 22:39:21 +0900

    Inf - session8/ @Bean(initMethod, destroyMethod)

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,8 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean , DisposableBean {
+//public class NetworkClient implements InitializingBean , DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -29,16 +30,27 @@ public class NetworkClient implements InitializingBean , DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override   // 의존관계 주입이 끝나면 호출 하겠다는 의미
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+//    @Override   // 의존관계 주입이 끝나면 호출 하겠다는 의미
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override   // 빈이 종료 될 때 호출
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        disconnect();
+//    }
+
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override   // 빈이 종료 될 때 호출
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
