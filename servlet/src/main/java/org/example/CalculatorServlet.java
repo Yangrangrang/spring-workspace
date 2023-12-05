126bde1e09df8b396ec343e6f1829aca36a5bd3f
yangrangrang <gkssk2309@gmail.com>
2023-12-05 20:37:33 +0900

    Fast - part1.Ch04.06 / servlet(2) /calculate servlet

---
+++ servlet/src/main/java/org/example/CalculatorServlet.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calculate")
+public class CalculatorServlet implements Servlet {
+    private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+    private ServletConfig servletConfig;
+
+    // 서블릿 인스턴스는 한번만 호출 되기 때문에 다음 사용자 요청이 들어왔을때 init메소드는 호출되지 않는다.
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        log.info("init");
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        log.info("service");
+
+        int operand1 = Integer.parseInt(req.getParameter("operand1"));
+        String operator = req.getParameter("operator");
+        int operand2 = Integer.parseInt(req.getParameter("operand2"));
+
+        System.out.println("operator = " + operator);
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = res.getWriter();
+        writer.println(result);
+    }
+
+    @Override
+    public void destroy() {
+        // resource release
+    }
+
+    // 기타 메소드
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.servletConfig;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+}
---
+++ servlet/src/main/java/org/example/WebApplicationServer.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class WebApplicationServer {
+
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    // webapplication server code
+    public static void main(String[] args) throws Exception {
+        // tomcat의 root directory 설정 (webapps)
+        String webappDirLocation = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        // tomcat의 port 8080 실행
+        tomcat.setPort(8080);
+
+        // 8080 port로 들어오면 루트디렉토리인 webapps 밑에서 찾는다
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+        /**
+         * command + ;
+         * main path >> /code_study/spring-workspace/servlet/webapps/WEB-INF/classes/java/main
+         */
+    }
+}
