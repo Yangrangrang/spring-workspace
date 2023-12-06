d1af78bc5065d883902c44af29b00c3223bde009
yangrangrang <gkssk2309@gmail.com>
2023-12-06 21:15:36 +0900

    Fast - part1.Ch05.03 / jdbc-pratice(1) h2-database errSolution

--- jdbc-pratice/src/test/resources/db_schema.sql
+++ jdbc-pratice/src/test/resources/db_schema.sql
@@ -6,6 +6,6 @@ CREATE TABLE USERS
     userId   varchar(12) not null,
     password varchar(12) not null,
     name     varchar(20) not null,
-    email    varchar(50)
-        primary key (userId);
+    email    varchar(50),
+        primary key (userId)
 );
