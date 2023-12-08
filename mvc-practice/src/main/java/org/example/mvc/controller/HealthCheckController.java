62e8cbbf19a313e5a75f3b3a832a005f32041d72
yangrangrang <gkssk2309@gmail.com>
2023-12-08 19:33:47 +0900

    Fast - part1.Ch06.01 / mvc-pratice(2) - reflections (Controller,Service)

---
+++ mvc-practice/src/main/java/org/example/mvc/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package org.example.mvc.controller;
+
+import org.example.mvc.annotation.Controller;
+import org.example.mvc.annotation.RequestMapping;
+import org.example.mvc.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class HealthCheckController {
+
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public String home(HttpServletResponse res, HttpServletRequest req) {
+        return "ok";
+    }
+}
