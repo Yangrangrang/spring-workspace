4c6c2fb21e51beb69c8743a5644c3e2c52e5dc2c
yangrangrang <gkssk2309@gmail.com>
2023-12-08 19:34:10 +0900

    Fast - part1.Ch06.01 / mvc-pratice(2) - reflections (Controller,Service) refactoring

--- mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -7,7 +7,9 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,14 +21,25 @@ public class ReflectionTest {
     @Test
     void controllerScan() {
 
-        // ord.example 패키지 밑에 있는 모든 클래스 대상으로 reflection을 사용
+//        // ord.example 패키지 밑에 있는 모든 클래스 대상으로 reflection을 사용
+//        Reflections reflections = new Reflections("org.example");
+
+//        Set<Class<?>> beans = getTypesAnnotatedWith();
+        // 해당 패키지 밑에 Controller라는 어노테이션이 붙어져 있는 대상들을 다 찾아서 해당 hashSet에 담는다.
+//        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+//        beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
+
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+        logger.debug("beans: [{}]", beans);
+    }
+
+    private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations ) {
+
         Reflections reflections = new Reflections("org.example");
 
         Set<Class<?>> beans = new HashSet<>();
-        // 해당 패키지 밑에 Controller라는 어노테이션이 붙어져 있는 대상들을 다 찾아서 해당 hashSet에 담는다.
-        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
-        beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
 
-        logger.debug("beans: [{}]", beans);
+        return beans;
     }
 }
