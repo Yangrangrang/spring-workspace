e76eae3face03304329e5d497bc1db913cd27021
yangrangrang <gkssk2309@gmail.com>
2023-12-12 19:59:23 +0900

    Fast - part1.Ch06.05 / mvc-pratice2(2) - HandlerKey

--- mvc-practice2/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
+++ mvc-practice2/src/main/java/org/example/mvc/RequestMappingHandlerMapping.java
@@ -1,20 +1,21 @@
 package org.example.mvc;
 
-import org.example.mvc.controller.Controller;
-import org.example.mvc.controller.HomeController;
+import org.example.mvc.controller.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RequestMappingHandlerMapping {
     // 예를들어 , /users[keys]라는 경로로 들어왔을 때 UserController[value]를 실행해줘 (key,value 형태로 저장하기 위해서 저장 관리하는 클래스)
-    private Map<String, Controller> mappings = new HashMap<>();
+    private Map<HandlerKey, Controller> mappings = new HashMap<>();
 
     void init() {
-        mappings.put("/", new HomeController());
+        mappings.put(new HandlerKey(RequestMethod.GET, "/"), new HomeController());
+        mappings.put(new HandlerKey(RequestMethod.GET, "/users"), new UserListController());
+//        mappings.put(new HandlerKey(RequestMethod.POST, "/users"), new UserListController());
     }
 
-    public Controller findHandler(String uriPath) {
-        return mappings.get(uriPath);
+    public Controller findHandler(HandlerKey handlerKey) {
+        return mappings.get(handlerKey);
     }
 }
