fdf14f7bb3038fc71dfb05ae569548ea53d8ba2a
eYanghanna <yhnn2131@emoney.co.kr>
2024-01-25 16:00:03 +0900

    #12 - 도메인 코드를 엔티티로 변환 1 도메인 클래스 == 1 테이블 매칭되도록 설계했다. 생성자와 `equals()`, `hashcode()` 등이 도메인 맥락에 맞게끔 디테일하게 설정함

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -10,15 +10,17 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
-        @Index(columnList = "createAt"),
-        @Index(columnList = "createBy")
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
 })
 @Entity
 public class Article {
@@ -31,6 +33,12 @@ public class Article {
     @Setter @Column(nullable = false, length = 10000) private String content;   // 내용
 
     @Setter private String hashtag;     // 해시태그
+
+    // 양방향 바인딩
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;             // 생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy;        // 생성자
