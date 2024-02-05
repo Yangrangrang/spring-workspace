c8782a218e63af90ed2b6153ad51cf1d14efbbec
yangrangrang <gkssk2309@gmail.com>
2024-02-05 21:05:26 +0900

    #29 - 게시글 페이지 서비스 로직의 테스트와 골격 잡기
    게시글 기준으로 생성, 수정, 삭제

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -2,11 +2,12 @@ package com.fastcampus.projectboard.service;
 
 import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
+import com.fastcampus.projectboard.dto.ArticleUpdateDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,13 +19,22 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
-    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
         return Page.empty();
     }
 
-    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public ArticleDto searchArticle(long l) {
         return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
     }
 }
