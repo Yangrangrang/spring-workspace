dfec398c5302f1f143d9415011f84ffbd1d91ce6
yangrangrang <gkssk2309@gmail.com>
2024-02-07 14:31:23 +0900

    #30 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성 강의에서 다루지 않은 내용 git에서 확인하여 작성 연관된 DTO 재설계하고 테스트가 필요로 하는 repository 쿼리 메소드도 작성
    아직 서비스 로직을 작성하기 전이여서 테스트는 실패됨

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -2,10 +2,10 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
-import com.querydsl.core.types.dsl.DateExpression;
 import com.querydsl.core.types.dsl.DateTimeExpression;
-import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -30,4 +30,6 @@ public interface ArticleRepository extends
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);  // 문자열x DateTimeExpression 로 변경
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
+
+    Page<Article> findByTitle(String title, Pageable pageable);
 }
