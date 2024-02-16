4637357a1895998d344a999df28bf22be782600f
yangrangrang <gkssk2309@gmail.com>
2024-02-16 11:12:35 +0900

    #45 - [게시판 검색 구현 - 해시태그2] - querydsl 이전 강의에서 만든 `findAllDistinctHashtag` 메소드 구현 - querydsl로 작성

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,37 @@
+package com.fastcampus.projectboard.repository.querydsl;
+
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.jpa.JPQLQuery;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom {
+
+    public ArticleRepositoryCustomImpl() {
+        super(Article.class);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtag() {
+        QArticle article = QArticle.article;
+
+        /**
+         * 아래 두개의 표현으로 할수 있음
+         */
+
+//        return from(article)
+//                .distinct()
+//                .select(article.hashtag)
+//                .where(article.hashtag.isNotNull())
+//                .fetch();
+
+        JPQLQuery<String> query = from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull());
+
+        return (List<String>) query;
+    }
+}
