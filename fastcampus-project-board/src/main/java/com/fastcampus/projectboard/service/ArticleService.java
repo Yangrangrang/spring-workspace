47ea8a8106bfd6a413c681ac1ba2541d1e3ba42f
yangrangrang <gkssk2309@gmail.com>
2024-02-25 22:11:25 +0900

    #53 - [인증 기능 구현 -3] 수정부분이 제대로 동작하지 않음.
    인증을 구현하고 수정하는 부분이 제대로 동작하지 않아서 확인해보니,
    .equels()에서 false이 되고 있었음.
    이 부분 수정

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -77,12 +77,12 @@ public class ArticleService {
             UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
 
             if (article.getUserAccount().equals(userAccount)) {
-                if (dto.title() != null) { article.setTitle(dto.title());}
-                if (dto.content() != null) { article.setContent(dto.content());}
+                if (dto.title() != null) { article.setTitle(dto.title()); }
+                if (dto.content() != null) { article.setContent(dto.content()); }
                 article.setHashtag(dto.hashtag());
             }
         } catch (EntityNotFoundException e) {
-            log.warn("게시글 없데이트 실패. 게시글을 수정하는데 필요한 정보를 찾을 수 없습니다. - {}", e.getLocalizedMessage());
+            log.warn("게시글 업데이트 실패. 게시글을 수정하는데 필요한 정보를 찾을 수 없습니다. - {}", e.getLocalizedMessage());
         }
     }
 
