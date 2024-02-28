74aa66d5473e293342093df9ebb7ec4bb0da4aaa
yangrangrang <gkssk2309@gmail.com>
2024-02-28 10:11:27 +0900

    #57 - 불필요한 import ,띄어쓰기 제거 (전체 테스트 통화 확인)

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleCommentController.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/controller/ArticleCommentController.java
@@ -1,13 +1,11 @@
 package com.fastcampus.projectboard.controller;
 
-import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.dto.request.ArticleCommentRequest;
 import com.fastcampus.projectboard.dto.security.BoardPrincipal;
 import com.fastcampus.projectboard.service.ArticleCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
