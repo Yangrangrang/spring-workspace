f73730e5361be182eefdf638b402c1bd99a51056
yangrangrang <gkssk2309@gmail.com>
2024-02-29 17:29:50 +0900

    #58 - Spring Boot Security Auto Configuration DOC에 있는 내용으로만 썼을때, There is no PasswordEncoder mapped for the id "null" 에러가 나서 비밀번호 `passwordEncoder` 부분을 임의로 추가함.

---
+++ spring-security-1/src/main/java/com/example/springsecurity1/config/DefaultSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.springsecurity1.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class DefaultSecurityConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(UserDetailsService.class)
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        String generatedPassword = passwordEncoder().encode("123");
+        return new InMemoryUserDetailsManager(User.withUsername("user")
+                .password(generatedPassword).roles("USER").build());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
+    DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegete) {
+        return new DefaultAuthenticationEventPublisher(delegete);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
