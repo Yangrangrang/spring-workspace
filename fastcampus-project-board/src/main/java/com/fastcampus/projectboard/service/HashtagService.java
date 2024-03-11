c2c08e1311165e1b1dd663595a8df3a24860bbae
yangrangrang <gkssk2309@gmail.com>
2024-03-11 14:53:12 +0900

    #68 - 해시태그 서비스 로직 수정, 테스트 추가
    게시글에 달린 해시태그를 관리하기 위한 기능을 구현,
    독특하게 해시태그의 저장을 다루는 메소드가 없는데
    해시태그의 저장은 현재 비즈니스 로직의 구조 상 게시글이 저장, 수정 될 때만 일어나는 기능으로 연관관계 매핑을 통해 게시글 서비스에서 모두 처리가 되기 때문
    
    * 해시태그 이름으로 해시태그 정보를 db조회하기
    * 본문에서 해시태그 파싱하기
    * 게시글이 더 이상 없는 해시태그를 삭제하기
    * TODO: 삭제 기능의 테스트를 강의중에 만들지 않았다. 추가하면 좋을 듯.

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/HashtagService.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/HashtagService.java
@@ -1,16 +1,17 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Hashtag;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class HashtagService {
-    public Object parseHashtagNames(String content) {
+    public Set<String> parseHashtagNames(String content) {
         return null;
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
+    public Set<Hashtag> findHashtagsByNames(Set<String> expectedHashtagNames) {
         return null;
     }
 
