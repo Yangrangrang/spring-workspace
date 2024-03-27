7b9ffdb8f9e25793b754ea5f7bcd0f1bd69f4eb4
yangrangrang <gkssk2309@gmail.com>
2024-03-27 13:23:02 +0900

    #85 - BoardPrincipal: OAuth 인증 정보를 다룰 수 있도록 기능 추가
    `OAuth2User` 인터페이스를 구현해서 Spring OAuth Client 도 기존과 동일하게 인증 정보를 인식할 수 있도록 함.

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -31,7 +38,8 @@ public record BoardPrincipal(
                         .collect(Collectors.toUnmodifiableSet()),
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -55,7 +63,7 @@ public record BoardPrincipal(
         );
     }
 
-    // 권한
+    // 권한 Spring Security (UserDetails)
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
@@ -90,6 +98,14 @@ public record BoardPrincipal(
     public boolean isEnabled() {
         return true;
     }
+
+
+    // OAuth
+    @Override
+    public Map<String, Object> getAttributes() { return oAuth2Attributes; }
+
+    @Override
+    public String getName() { return username; }    // 유저 식별정보
 
     public enum RoleType {
         USER("ROLE_USER");
