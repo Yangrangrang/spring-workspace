59d8573a17b8e5fb773dffb034cebc9b7dbf0e14
yangrangrang <gkssk2309@gmail.com>
2024-04-26 17:19:50 +0900

    #58 - Dto추가

---
+++ spring-security-1/src/main/java/com/example/springsecurity1/dto/UserAccountDto.java
@@ -0,0 +1,63 @@
+package com.example.springsecurity1.dto;
+
+import com.example.springsecurity1.domain.UserAccount;
+
+import java.time.LocalDateTime;
+
+// record클래스는 불변 데이터를 객체 간에 전달하는 작업을 간단하게 만들어준다.
+// 특징 : 멤버변수는 private final로 선언, 필드별 getter 자동 생성, 모든 멤버변수를 인자로 하는 public 생성자를 자동 생성
+//       equals, hashcode, toString을 자동 생성, 기본생성자는 제공하지 않아 필요한 경우 직접 생성
+public record UserAccountDto(
+        Long id,
+        String userId,
+        String userPassword,
+        String nickName,
+        String email,
+        String phone,
+        String isDelete,
+        LocalDateTime lastLogin,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+
+    public static UserAccountDto of(String userId, String userPassword, String nickName, String email, String phone) {
+        return new UserAccountDto(null, userId, userPassword, nickName, email, phone, "N", null, null, null, null, null);
+    }
+
+    public static UserAccountDto of(Long id, String userId, String userPassword, String nickName, String email, String phone, String isDelete, LocalDateTime lastLogin, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(id, userId, userPassword, nickName, email, phone, isDelete, lastLogin, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    // DB에서 가져온 entity를 dto로 전환
+    public static UserAccountDto from(UserAccount entity) {
+        return new UserAccountDto(
+                entity.getId(),
+                entity.getUserId(),
+                entity.getUserPassword(),
+                entity.getNickName(),
+                entity.getEmail(),
+                entity.getPhone(),
+                entity.getIsDelete(),
+                entity.getLastLogin(),
+                entity.getCreateAt(),
+                entity.getCreateBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    // DTO를 entity로 전환
+    public UserAccount toEntity() {
+        return UserAccount.of(
+                userId,
+                userPassword,
+                nickName,
+                email,
+                phone,
+                isDelete,
+                lastLogin
+        );
+    }
+}
