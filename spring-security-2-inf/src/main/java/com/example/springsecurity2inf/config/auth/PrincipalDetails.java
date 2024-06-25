e143df3048aac61c7469d117e6b712b31efc7cee
yangrangrang <gkssk2309@gmail.com>
2024-06-25 16:36:29 +0900

    #105 - Authentication이 가질 수 있는 2개 객체 - UserDetails, OAuth2User
    > 일반 로그인은 UserDetails , OAuth2를 통한 로그인은 OAuth2User 객체
    > 세션에 저장 타입이 달라서 이 둘을 함께 implement한 PrincipalDetails
    > 기존에 있던 PrincipalDetails에 OAuth2User implement하면, getName, getAttribute 2개 메소드 생성

--- spring-security-2-inf/src/main/java/com/example/springsecurity2inf/config/auth/PrincipalDetails.java
+++ spring-security-2-inf/src/main/java/com/example/springsecurity2inf/config/auth/PrincipalDetails.java
@@ -1,11 +1,14 @@
 package com.example.springsecurity2inf.config.auth;
 
 import com.example.springsecurity2inf.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * 시큐리티가 /login 주소요청이 오면 낚아채서 로그인을 진행시킨다.
@@ -17,7 +20,9 @@ import java.util.Collection;
  *
  * Security Session => Authentication => UserDetail(PrincipalDetails)
  */
-public class PrincipalDetails implements UserDetails {
+
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;  // 콤포지션
 
@@ -37,7 +42,6 @@ public class PrincipalDetails implements UserDetails {
         });
         return collect;
     }
-
     @Override
     public String getPassword() {
         return user.getPassword();
@@ -49,23 +53,24 @@ public class PrincipalDetails implements UserDetails {
     }
 
     //
+
     @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
     }
-
     // 계정 잠겼네
+
     @Override
     public boolean isAccountNonLocked() {
         return UserDetails.super.isAccountNonLocked();
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
         return UserDetails.super.isCredentialsNonExpired();
     }
 
     // 계정활성화
+
     @Override
     public boolean isEnabled() {
 
@@ -73,5 +78,14 @@ public class PrincipalDetails implements UserDetails {
         // 현재시간 - 로그인시간 => 1년 초과시, return false
 
         return UserDetails.super.isEnabled();
+    }
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
     }
 }
--- spring-security-2-inf/src/main/java/com/example/springsecurity2inf/config/oauth/PrincipalOauth2UserService.java
+++ spring-security-2-inf/src/main/java/com/example/springsecurity2inf/config/oauth/PrincipalOauth2UserService.java
@@ -13,10 +13,13 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         System.out.println("userRequest: " + userRequest);
-        System.out.println("userRequest.getClientRegistration() = " + userRequest.getClientRegistration());
+        System.out.println("userRequest.getClientRegistration() = " + userRequest.getClientRegistration()); // registrational로 어떤 OAuth로 로그인했는 지 확인 가능
         System.out.println("userRequest.getAccessToken().getTokenValue() = " + userRequest.getAccessToken().getTokenValue());
+        // 구글 로그인버튼 클릭 ->  구글 로그인창 -> 로그인을 완료 -> code 반환(OAuth-Client라이브러리) -> AccessToken요청
+        // userRequest정보 -> loadUser함수 호출 -> 구글로 부터 회원프로필 받아준다.
         System.out.println("super.loadUser(userRequest).getAttributes() = " + super.loadUser(userRequest).getAttributes());
 
+        OAuth2User oAuth2User = super.loadUser(userRequest);
         // 회원가입을 강제로 진행해볼 예정
         return super.loadUser(userRequest);
     }
