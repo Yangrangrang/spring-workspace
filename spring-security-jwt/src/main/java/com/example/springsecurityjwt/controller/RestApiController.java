8b87da544d989afe13959973fb385b9764727120
yangrangrang <gkssk2309@gmail.com>
2024-07-08 19:33:20 +0900

    #107 - controller 누락 추가,  BCryptPasswordEncoder bean 등록

--- spring-security-jwt/src/main/java/com/example/springsecurityjwt/controller/RestApiController.java
+++ spring-security-jwt/src/main/java/com/example/springsecurityjwt/controller/RestApiController.java
@@ -1,13 +1,23 @@
 package com.example.springsecurityjwt.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springsecurityjwt.model.User;
+import com.example.springsecurityjwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class RestApiController {
 
-    @GetMapping("/home")
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 모든 사람이 접근 가능
+    @GetMapping("home")
     public String home() {
         return "<h1>home</h1>";
     }
@@ -15,5 +25,25 @@ public class RestApiController {
     @PostMapping("/token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    // 매니저 혹은 어드민이 접근 가능
+    @GetMapping("manager/reports")
+    public String reports() {
+        return "<h1>reports</h1>";
+    }
+
+    // 어드민이 접근 가능
+    @GetMapping("admin/users")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("join")
+    public String join(@RequestBody User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles("ROLE_USER");
+        userRepository.save(user);
+        return "회원가입완료";
     }
 }
