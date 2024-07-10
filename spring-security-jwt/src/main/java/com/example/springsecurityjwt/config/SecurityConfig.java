17ccc5c97484e35d3ce44c9efd284e86a9ccd374
yangrangrang <gkssk2309@gmail.com>
2024-07-10 21:05:38 +0900

    #107 - JWT를 위한 강제 로그인
    `JwtAuthenticationFilter` 필터에서 요청받은 username 과 password 확인 하고, 토큰 만들어서 `authenticaionManager` 를 통해
    `loadUserByUsername` 함수가 실행 되어 username , password 확인하고 로그인이 정보가 담기면, authenricaion 객체가 session영역에 저장됨. 이는 로그인이 되었다는 뜻,
    
    `SecurityConfig` 에서 `authenticaionManager`를 `http.getSharedObject(AuthenticaionManager.class)`로 인자값을 넘기면
    `Cannot invoke "org.springframework.security.authentication.AuthenticationManager.authenticate(org.springframework.security.core.Authentication)" because "this.authenticationManager" is null` 에러 확인 되어서
    빈 등록.

--- spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
+++ spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    // authenticaionManager 가 null 에러 이슈
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -36,7 +43,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 //        http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);   // 필터 걸때 이렇게 할 필요는 없다.
         http.addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class); // 시큐리티 필터가 우선.
         http.csrf(csrf -> csrf.disable());
@@ -44,7 +51,7 @@ public class SecurityConfig {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(login -> login.disable())
                 .httpBasic(basic -> basic.disable())
-                .addFilter(new JwtAuthenticationFilter(http.getSharedObject(AuthenticationManager.class)))   // AuthenticationManager 를 던져줘야함.
+                .addFilter(new JwtAuthenticationFilter(authenticationManager))   // AuthenticationManager 를 던져줘야함.
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/user/**").hasAnyAuthority("ROLE_USER", "ROLE_MANAGER", "ROLE_ADMIN")
                         .requestMatchers("/api/v1/manager/**").hasAnyAuthority("ROLE_MANAGER", "ROLE_ADMIN")
--- spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/jwt/JwtAuthenticationFilter.java
+++ spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,12 +1,21 @@
 package com.example.springsecurityjwt.config.jwt;
 
+import com.example.springsecurityjwt.config.auth.PrincipalDetails;
+import com.example.springsecurityjwt.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 
 /**
  * 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter 가 있음.
@@ -24,6 +33,46 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         System.out.println("JWTAuthenticationFilter : 로그인 시도중");
 
         // 1. username, password 받아서
+        try {
+            /**
+             BufferedReader br = request.getReader();
+
+             String input = null;
+             while ((input = br.readLine()) != null) {
+             System.out.println("input = " + input);  // username=test&password=1234
+             }
+             */
+
+            // 위 방법 보다 더 편하게
+            ObjectMapper om = new ObjectMapper();
+            User user = om.readValue(request.getInputStream(), User.class);
+            System.out.println("user = " + user);
+
+            // 토큰 만들기
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+            System.out.println("authenticationToken = " + authenticationToken);
+
+            // PrincipalDetailsService 의 loadUserByUsername() 함수가 실행된 후 정상이면 authentication 이 리턴 됨.
+            // DB 에 있는 username 과 password 가 일치
+            // authentication 에 내 로그인한 정보가 담김.
+            System.out.println("authenticationManager = " + authenticationManager);
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            // authentication 객체가 session 영역에 저장됨. => 로그인이 되었다는 뜻
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+            System.out.println("principalDetails.getUsername = " + principalDetails.getUsername());
+            System.out.println("principalDetails.getPassword = " + principalDetails.getPassword());
+
+            return authentication;  // session 영역에 저장을 해야하고 그 방법이 return 해주면 됨.
+            // 리턴의 이유는 권한 관리를 security 가 대신 해주기 때문에 편하려고 하는거임.
+            // 굳이 JWT 토큰을 사용하면서 세션을 만들 이유가 없음. 근데 단지 권한 처리 때문에 session 에 넣어줌.
+
+//            System.out.println(request.getInputStream().toString());    // 유저네임과 패스워드가 담겨 있다.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("===================================");
 
         // 2. 정상인지 로그인 시도를 해보는 거에요. authenticationManager 로 로그인 시도를 하면
         // PrincipalDetailsService 가 호출되서 자동으로 loadUserByUsername() 함수 실행됨.
@@ -31,6 +80,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         // 3. PrincipalDetails 를 세션에 담고 (세션에 담지 않으면 권한 관리가 되지 않음..권한 관리를 위해서 이기때문에 권한이 따로 없다면 세션에 안담아도 됨.)
 
         // 4. JWT 토킁을 만들어서 응답해주면 됨.
-        return super.attemptAuthentication(request, response);
+        return null;
+    }
+
+    /**
+     * attemptAuthentication() 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행 됨.
+     * JWT 토큰을 만들어서 request 요청한 사용자에게 JWT 토큰을 response 해주면 됨.
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        System.out.println("successfulAuthentication 실행 됨 : 인증이 완료 되었다는 뜻.");
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
